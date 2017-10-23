a61f575607c37ce81a9927585f3b729da8cd16c6
Digitroinc <craigbett@gmail.com>
2017-10-22 21:17:34 -0400

    Update

--- prop_hunt/gamemode/cl_init.lua
+++ prop_hunt/gamemode/cl_init.lua
@@ -1,10 +1,1 @@
----
---- cl_init.lua - Client Init Component
------------------------------------------------------
---- init file for client
----
-
---
--- Include shared init
---
-include('sh_init.lua')
+DeriveGamemode('lastprop')
--- prop_hunt/gamemode/init.lua
+++ prop_hunt/gamemode/init.lua
@@ -1,13 +1,2 @@
----
---- init.lua - Server Init Component
------------------------------------------------------
---- init file for server
----
-
-AddCSLuaFile('sh_init.lua')
 AddCSLuaFile('cl_init.lua')
-
---
--- Include shared server init
---
-include('sh_init.lua')
+DeriveGamemode('lastprop')
