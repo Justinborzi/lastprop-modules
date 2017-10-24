23a2f4d1ebe116ff97dea9b5cbee7c78a37e3771
Digitroinc <craigbett@gmail.com>
2017-10-24 13:38:46 -0400

    Update

--- lua/lastprop/modules/config/sv_database.lua
+++ lua/lastprop/modules/config/sv_database.lua
@@ -2,13 +2,14 @@
 Example Database Config:
 ------------------------------------------------------------
 GM.db = {
-    enabled    = true
-    prefix     = 'lps_'
-    username   = 'root'
-    password   = ''
-    database   = 'lps'
-    host       = '127.0.0.1'
-    unixsocket = ''
-    module     = 'sqlite'
+    enabled    = true,
+    prefix     = 'lps_',
+    username   = 'root',
+    password   = '',
+    database   = 'lps',
+    host       = '127.0.0.1',
+    unixsocket = '',
+    port       = 3306,
+    module     = 'sqlite',
 }
 ---------------------------------------------------------]]--
