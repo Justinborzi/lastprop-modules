b0267bd26ebd7358fd32b99aaeaacda90b77f91c
Digitroinc <craigbett@gmail.com>
2017-10-27 11:55:05 -0400

    update

--- lua/ps2/modules/lastprop_integration/sh_prevent_visuals.lua
+++ lua/ps2/modules/lastprop_integration/sh_prevent_visuals.lua
@@ -1,8 +1,15 @@
-local function hideOnProps(ply)
+local function visuals(ply)
 	if ply:IsSpec() or ply:IsDisguised() or not ply:Alive() then
 		return false
 	end
 end
-hook.Add( "PS2_VisualsShouldShow", "OnlyHunters", hideOnProps )
-hook.Add( "PS2_PlayermodelShouldShow", "OnlyHunters", hideOnProps )
-hook.Add( "PS2_WeaponShouldSpawn", "OnlyHunters", hideOnProps )
+
+local function swep(ply)
+	if ply:IsSpec() or not ply:Alive() or (ply:Team() == TEAM.PROPS and not ply:IsLastMan() and (GAMEMODE:InPreRound() or GAMEMODE:InRound())) then
+		return false
+	end
+end
+
+hook.Add( "PS2_VisualsShouldShow", "LPS:PS2_VisualsShouldShow", visuals )
+hook.Add( "PS2_PlayermodelShouldShow", "LPS:PS2_PlayermodelShouldShow", visuals )
+hook.Add( "PS2_WeaponShouldSpawn", "LPS:PS2_WeaponShouldSpawn", swep )
