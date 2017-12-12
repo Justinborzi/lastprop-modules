d2807512958c84302d76beacdfb3d910ba0fbb8a
MysticSeagull <Justinborzi@gmail.com>
2017-12-12 17:30:55 -0500

    Update sh_tauntpack.lua

--- lua/lastprop/modules/config/sh_tauntpack.lua
+++ lua/lastprop/modules/config/sh_tauntpack.lua
@@ -1,10 +1,10 @@
 --[[---------------------------------------------------------
 Example Taunt Pack:
 ------------------------------------------------------------
- - name, a unique name for the taunt, no spaces, best to use the fileanme
+ - name, a unique name for the taunt, no spaces, best to use the filename.
  - label, the human readable name for the taunt, (shows up in the taunt menu)
- - file, the mp3 or wav file for the taunt
- - length, how long the taunt is in seconds. It can be a float so 1.5 is ok
+ - file, the mp3 or wav file for the taunt.
+ - length, how long the taunt is in seconds. It can be a float value for example, 1.5 is ok.
 
 hook.Add('Initialize', 'MyTauntPack', function()
 	GAMEMODE:RegisterTauntPack('MyTauntPack', {
