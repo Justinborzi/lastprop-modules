c35d192b1edd4e4b675f6ede96195b9f7a9c535c
MysticSeagull <Justinborzi@gmail.com>
2017-12-12 17:35:13 -0500

    Update ulx_mapvote.lua

--- lua/ulx/modules/sh/ulx_mapvote.lua
+++ lua/ulx/modules/sh/ulx_mapvote.lua
@@ -1,14 +1,14 @@
 function ulx.mapvote(calling_ply, time, opposite)
     if opposite then
         lps.mapvote:Cancel()
-        ulx.fancyLogAdmin(calling_ply, "#A stopped the mapvote!")
+        ulx.fancyLogAdmin(calling_ply, "#A has stopped the mapvote!")
     else
         lps.mapvote:Start(voteTime)
-        ulx.fancyLogAdmin(calling_ply, "#A started a mapvote!")
+        ulx.fancyLogAdmin(calling_ply, "#A has started a mapvote!")
     end
 end
 
 function ulx.rtvreset(calling_ply)
     lps.mapvote:ResetRTV()
-    ulx.fancyLogAdmin(calling_ply, "#A reset the RTV.")
+    ulx.fancyLogAdmin(calling_ply, "#A has reset the RTV.")
 end
